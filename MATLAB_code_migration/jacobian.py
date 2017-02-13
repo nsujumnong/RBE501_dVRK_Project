3627b20cc961efb03c450040c1a343aeb6397368
Nuttaworn <nsujumnong@wpi.edu>
2017-02-13 14:58:02 -0500

    updated newtonEuler.py and jacobian.py

--- MATLAB_code_migration/jacobian.py
+++ MATLAB_code_migration/jacobian.py
@@ -32,20 +32,80 @@ def jacobian(f,v):
 	
 	
 
-#L = range(10)
-#print L
-#R = L[:2]
-#print R
+def angularJacobian(i,dh_table,rho):
+	n = len(dh_table)
+	J_w = numpy.zeros((3,i))
+	for k in range (0,i):
+		T = numpy.identity(4)
+		for j in range (0,i):
+			theta = dh_table[j,0]
+			d = dh_table[j,1]
+			a = dh_table[j,2]
+			alpha = dh_table[j,3]
+			dhTrans_i = dhTransform(theta,d,a,alpha) 
+			T = numpy.dot(T,dhTrans_i)
+			#print "T", j+1
+			#print T
+		#print T
+		#print T[0:3,3]
+		#print rho[0:4]
+		J_w[:,k] = numpy.dot(rho[k],T[0:3,3])
+		#J_w[:,k] = numpy.transpose(J_w[:,k])
+	return J_w
 	
+#Testing
+#angJ_1 = angularJacobian(1,dh_table,rho)
+#angJ_2 = angularJacobian(2,dh_table,rho)
+angJ_3 = angularJacobian(3,dh_table,rho)
+#angJ_4 = angularJacobian(4,dh_table,rho)
+#print angJ_1
+#print angJ_2
+#print angJ_3
+#print angJ_4
 
-#x = np.array([[1,2,3],[4,5,6],[7,8,9]])
+def linearJacobian(i,dh_table):
+	n = len(dh_table)
+	J_v = numpy.zeros((3,i))
 	
-x,y,z = symbols('x y z', real=True)
-a,b,c = symbols('a b c', real=True)
-f = 4*x*y + x*sin(z) + x**3 + z**8*y
-
-#print diff(f,x)
-
-sin_pi = sin(pi/2*x)
-d_sin = diff(sin_pi,x)
-print d_sin
+	for k in range (0,i):
+		T = numpy.identity(4)
+		for j in range (0,i):
+			theta = dh_table[j,0]
+			d = dh_table[j,1]
+			a = dh_table[j,2]
+			alpha = dh_table[j,3]
+			dhTrans_i = dhTransform(theta,d,a,alpha)
+			T = numpy.dot(T,dhTrans_i)
+		p_i = T[0:3,3].reshape((3,1))
+		R_i = T[0:3,0:3]
+		k = numpy.array([[0],[0],[1]])
+		z_i = numpy.dot(R_i,k)
+		#print "p_i"
+		#print p_i
+		print "z_i"
+		print z_i, z_i.shape
+		#return p_i,z_i
+		
+		T_n = numpy.identity(4)
+		for l in range (0,n):
+			theta = dh_table[l,0]
+			d = dh_table[l,1]
+			a = dh_table[l,2]
+			alpha = dh_table[l,3]
+			dhTrans_n = dhTransform(theta,d,a,alpha)
+			T_n = numpy.dot(T_n,dhTrans_n)
+		#print "T_n"
+		#print T_n
+		p_n = T_n[0:3,3].reshape((3,1))
+		#print "p_n"
+		#print p_n	
+		r_i = p_n - p_i
+		print "r_i"
+		print r_i, r_i.shape
+		J_v[:,k] = numpy.cross(z_i,r_i)
+		return p_n, r_i#, J_v[:,k]
+		
+	return J_v
+	
+linJ_4 = linearJacobian(2,dh_table)
+#print linJ_4
--- MATLAB_code_migration/newtonEuler.py
+++ MATLAB_code_migration/newtonEuler.py
@@ -12,93 +12,109 @@ pi = numpy.pi
 #Testing DH table
 dh_table = numpy.array([[pi/6,2,3,0],[pi/4,5,10,pi/2],[pi/2,4,1,pi/2],[0,5,3,0]])
 rho = numpy.array([1,1,1,1])
-g = numpy.array([0,0,-9.81])
+
 #def newtonEuler()
 
-def angularJacobian(i,dh_table,rho):
-	n = len(dh_table)
-	J_w = numpy.zeros((3,i))
-	for k in range (0,i):
-		T = numpy.identity(4)
-		for j in range (0,i):
-			theta = dh_table[j,0]
-			d = dh_table[j,1]
-			a = dh_table[j,2]
-			alpha = dh_table[j,3]
-			dhTrans_i = dhTransform(theta,d,a,alpha) 
-			T = numpy.dot(T,dhTrans_i)
-			#print "T", j+1
-			#print T
-		#print T
-		#print T[0:3,3]
-		#print rho[0:4]
-		J_w[:,k] = numpy.dot(rho[k],T[0:3,3])
-		#J_w[:,k] = numpy.transpose(J_w[:,k])
-	return J_w
-	
-#Testing
-#angJ_1 = angularJacobian(1,dh_table,rho)
-#angJ_2 = angularJacobian(2,dh_table,rho)
-angJ_3 = angularJacobian(3,dh_table,rho)
-#angJ_4 = angularJacobian(4,dh_table,rho)
-#print angJ_1
-#print angJ_2
-#print angJ_3
-#print angJ_4
+#dh transformation check
+T1 = dhTransform(dh_table[0,0],dh_table[0,1],dh_table[0,2],dh_table[0,3])
+print "T1"
+print T1
 
-def linearJacobian(i,dh_table):
-	n = len(dh_table)
-	J_v = numpy.zeros((3,i))
-	
-	for k in range (0,i):
-		T = numpy.identity(4)
-		for j in range (0,i):
-			theta = dh_table[j,0]
-			d = dh_table[j,1]
-			a = dh_table[j,2]
-			alpha = dh_table[j,3]
-			dhTrans_i = dhTransform(theta,d,a,alpha)
-			T = numpy.dot(T,dhTrans_i)
-		p_i = T[0:3,3].reshape((3,1))
-		R_i = T[0:3,0:3]
-		k = numpy.array([[0],[0],[1]])
-		z_i = numpy.dot(R_i,k)
-		#print "p_i"
-		#print p_i
-		print "z_i"
-		print z_i, z_i.shape
-		#return p_i,z_i
-		
-		T_n = numpy.identity(4)
-		for l in range (0,n):
-			theta = dh_table[l,0]
-			d = dh_table[l,1]
-			a = dh_table[l,2]
-			alpha = dh_table[l,3]
-			dhTrans_n = dhTransform(theta,d,a,alpha)
-			T_n = numpy.dot(T_n,dhTrans_n)
-		#print "T_n"
-		#print T_n
-		p_n = T_n[0:3,3].reshape((3,1))
-		#print "p_n"
-		#print p_n	
-		r_i = p_n - p_i
-		print "r_i"
-		print r_i, r_i.shape
-		J_v[:,k] = numpy.cross(z_i,r_i)
-		return p_n, r_i#, J_v[:,k]
-		
-	return J_v
-	
-linJ_4 = linearJacobian(2,dh_table)
-#print linJ_4
+T2 = dhTransform(dh_table[1,0],dh_table[1,1],dh_table[1,2],dh_table[1,3])
+print "T2"
+print T2
 
-		
-	
-		
-		
 
+# Compute angular and linear velocity for force component
+
+def getRotMatrix(i,dh_table):
+	#get the rotation matrix of ith and ith+1 transformation matrix
+
+	#First, acquire the parameters of each link
+	for j in range (i,i+1):
+		#for ith link
+		theta_i = dh_table[j,0]
+		d_i = dh_table[j,1]
+		a_i = dh_table[j,2]
+		alpha_i = dh_table[j,3]
+		#for ith+1 link 
+		theta_ip1 = dh_table[j+1,0]
+		d_ip1 = dh_table[j+1,1]
+		a_ip1 = dh_table[j+1,2]
+		alpha_ip1 = dh_table[j+1,3]
+	#Get the ration matrices
+	T_i = dhTransform(theta_i,d_i,a_i,alpha_i)
+	R_i = T_i[0:3,0:3]
+	T_ip1 = dhTransform(theta_ip1,d_ip1,a_ip1,alpha_ip1)
+	R_ip1 = T_ip1[0:3,0:3]
+	return R_i, R_ip1  
+
+#function test
+R_i, R_ip1 = getRotMatrix(1,dh_table)
+print R_i
+print R_ip1
+
+def getPosMatrix(i,dh_table):
+	for j in range (i,i+1):
+		#for ith link
+		theta_i = dh_table[j,0]
+		d_i = dh_table[j,1]
+		a_i = dh_table[j,2]
+		alpha_i = dh_table[j,3]
+		#for ith+1 link 
+		theta_ip1 = dh_table[j+1,0]
+		d_ip1 = dh_table[j+1,1]
+		a_ip1 = dh_table[j+1,2]
+		alpha_ip1 = dh_table[j+1,3]
+	#Get the ration matrices
+	T_i = dhTransform(theta_i,d_i,a_i,alpha_i)
+	P_i = T_i[0:3,3]
+	T_ip1 = dhTransform(theta_ip1,d_ip1,a_ip1,alpha_ip1)
+	P_ip1 = T_ip1[0:3,3]
+	return P_i, P_ip1 
 	
+#function test
+P_i, P_ip1 = getPosMatrix(1,dh_table)
+print P_i
+print P_ip1
+
+def angularVelAc(dh_table, dq,ddq, joint_config):
 		
-		
+	n = len(dh_parameter)
+	g = 9.81			#gravitational acceleration (scalar)
 	
+	w = [[0],[0],[0]]		#ground angular velocity
+	dw = [[0],[0],[0]]		#ground angular acceleration
+	z = numpy.array[[0],[0],[1]]
+	
+	if len(joint_config) != len(dh_table):
+		print "cannot find solution"
+	
+	else:
+		for i in range (0,n):
+			R_i, R_ip1 = getRotMatrix(i,dh_table)
+			R = numpy.dot(R_i,R_ip1)
+			if joint_config[n] == 0:		#if joint is prismatic
+				w_ip1 = numpy.dot(R,w)
+				w = w_ip1
+				dw_ip1 = numpy.dot(R,dw)
+				dw = dw_ip1
+			elif joint_config[n] == 1:		#if joint is revolute
+				w_ip1 = numpy.dot(R,w)+numpy.dot(q[i],z)
+				w = w_ip1
+				dw_ip1 = numpy.dot(R,dw) + numpy.cross(numpy.dot(R,w),numpy.dot(dq,z)) + numpy.dot(dq,z)
+				dw = dw_ip1
+			else:
+				print "not a standard joint configuration"
+			
+	return w, dw
+	
+#def linearVelAc(dh_table, w):
+	
+
+
+
+
+
+
+
