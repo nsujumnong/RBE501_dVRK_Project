1bb94f1227be7f5096bebe5e987a522710728371
Nuttaworn <nsujumnong@wpi.edu>
2017-02-13 17:16:45 -0500

    updated newtonEuler.py: still need debuggin

--- MATLAB_code_migration/newtonEuler.py
+++ MATLAB_code_migration/newtonEuler.py
@@ -19,13 +19,13 @@ joint_config = numpy.array([1,1,1,1])
 #def newtonEuler()
 
 #dh transformation check
-T1 = dhTransform(dh_table[0,0],dh_table[0,1],dh_table[0,2],dh_table[0,3])
-print "T1"
-print T1
+#T1 = dhTransform(dh_table[0,0],dh_table[0,1],dh_table[0,2],dh_table[0,3])
+#print "T1"
+#print T1
 
-T2 = dhTransform(dh_table[1,0],dh_table[1,1],dh_table[1,2],dh_table[1,3])
-print "T2"
-print T2
+#T2 = dhTransform(dh_table[1,0],dh_table[1,1],dh_table[1,2],dh_table[1,3])
+#print "T2"
+#print T2
 
 
 # Compute angular and linear velocity for force component
@@ -34,7 +34,7 @@ def getRotMatrix(i,dh_table):
 	#get the rotation matrix of ith and ith+1 transformation matrix
 
 	#First, acquire the parameters of each link
-	for j in range (i,i+1):
+	for j in range (i-1,i):
 		#for ith link
 		theta_i = dh_table[j,0]
 		d_i = dh_table[j,1]
@@ -58,7 +58,7 @@ def getRotMatrix(i,dh_table):
 #print R_ip1
 
 def getPosMatrix(i,dh_table):
-	for j in range (i,i+1):
+	for j in range (i-1,i):
 		#for ith link
 		theta_i = dh_table[j,0]
 		d_i = dh_table[j,1]
@@ -90,7 +90,7 @@ def velAndAccel(dh_table, dq,ddq, joint_config):
 	dw_i = [[0],[0],[0]]		#ground angular acceleration
 	v_i = [[0],[0],[0]]		#initial linear velocity
 	dv_i = [[0],[0],[-g]]		#initial linear acceleration (gravitational acceleration)
-	z = numpy.array[[0],[0],[1]]
+	z = [[0],[0],[1]]
 	
 	if len(joint_config) != len(dh_table):
 		print "cannot find solution"
@@ -100,34 +100,48 @@ def velAndAccel(dh_table, dq,ddq, joint_config):
 			R_i, R_ip1 = getRotMatrix(i,dh_table)
 			R = numpy.dot(R_i,R_ip1)
 			P_i, P_ip1 = getPosMatrix(i,dh_table)
-			P = numpy.dot(P_i,P_ip1)
-			if joint_config[i] == 0:		#if joint is prismatic
-				w_ip1 = numpy.dot(R,w)
-				w_i = w_ip1
-				dw_ip1 = numpy.dot(R,dw)
-				dw_i = dw_ip1
+			#print "P_", i+1
+			#print P_i
+			#print "P_", i+2
+			#print P_ip1
 				
+			P = numpy.dot(P_i,P_ip1)
+			#print "P_",i+1,"_",i+2
+			#print P
+			
+			if joint_config[i] == 0:		#if joint is prismatic
+				w_ip1 = numpy.dot(R,w_i)
+				w_i = w_ip1
+				dw_ip1 = numpy.dot(R,dw_i)
+				dw_i = dw_ip1
 			elif joint_config[i] == 1:		#if joint is revolute
 				#angular velocity
-				w_ip1 = numpy.dot(R,w)+numpy.dot(q[i],z)
+				w_ip1 = numpy.dot(R,w_i)+numpy.dot(dq[i],z)
 				w_i = w_ip1
+				#print "w_",i+1
+				#w_i_T = w_i.T
+				#print w_i_T
+				#print w_i_T.shape
+				#print "dw_", i
+				#print dw_i
 				#angular acceleration
-				dw_ip1 = numpy.dot(R,dw) + numpy.cross(numpy.dot(R,w),numpy.dot(dq[i],z)) + numpy.dot(ddq[i],z)
+				dw_ip1 = numpy.dot(R,dw_i) + numpy.cross(numpy.dot(R,w_i).T,numpy.dot(dq[i],z).T) + numpy.dot(ddq[i],z)
 				dw_i = dw_ip1
 				
 				#linear velocity
-				comp1 = numpy.cross(w_i,P)
-				comp2 = numpy.cross(w_i,P)+numpy.cross(w_i,comp1)+dv_i
-				dv_ip1 = numpy.dot(R,comp2)
-				dv_i = dv_ip1
+				#comp1 = numpy.cross(w_i.T,P.T)
+				#comp2 = numpy.cross(w_i.T,P.T)+numpy.cross(w_i.T,comp1)+dv_i
+				#dv_ip1 = numpy.dot(R,comp2)
+				#dv_i = dv_ip1
 			else:
 				print "not a standard joint configuration"
-			
-	return w_i, dw_i, dv_i
+	
+	return w_i		
+	#return w_i, dw_i, dv_i
 	
 #algorithm test
-w_1, dw_1, dv_1 = velAndAccel()
-
+#w_1, dw_1, dv_1 = velAndAccel(dh_table,dq,ddq,joint_config)
+w_1 = velAndAccel(dh_table,dq,ddq,joint_config)
 
 #def linearVelAc(dh_table, joint_config):
 #	n = len(dh_table)
