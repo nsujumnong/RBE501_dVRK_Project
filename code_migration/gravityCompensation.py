bf2186ee07b229ed6df941ea8b9c891faa7496b7
nsujumnong <nsujumnong@wpi.edu>
2017-02-28 11:48:40 -0500

    rename MATLAB_code_migration to code_migration

--- code_migration/gravityCompensation.py
+++ code_migration/gravityCompensation.py
@@ -12,7 +12,7 @@ from newtonEulerMTM import *
 #compute the position error
 
 #since this algorithm is based on velocity and acceleration, we must limit those varables to 0
-#in order to maintain the current configuration (compensat the gravity)
+#in order to maintain the current configuration (compensate the gravity)
 
 #subscribe for desired position here
 def gravityCompensation():
@@ -31,17 +31,8 @@ def gravityCompensation():
 
 	pub_tor = rospy.Publisher('/dvrk_mtm/set_joint_effort',Float64MultiArray,queue_size=10)
 
-
 	#initiate position error
 	position_error = numpy.zeros((8,100))
-
-	for i in range 0:99:
-		#get current position by subscribing to '/dvrk_mtm/joint_position_current' here
-
-		torque = newtonEuler(dh_table,dq,ddq,joint_config)
-		#implement torque by publishing to '/dvrk_mtm/set_joint_effort' here
-		#need delay factor too
-		position_error[0:7,i] = #current position - desired position
 
 	for i in range (0,99):
 		#get current position by subscribing to '/dvrk_mtm/joint_position_current' here
@@ -51,15 +42,5 @@ def gravityCompensation():
 		#need delay factor too
 		rospy.sleep(0.01)
 		position_error[0:7,i] = #current position - desired position
-
-def set_torque(position,torque):
-
-	msg.Effort[0] = torque[0]
-	msg.Effort[1] = torque[1]
-	msg.Effort[2] = torque[2]
-	msg.Effort[3] = torque[3]
-	msg.Effort[4] = torque[4]
-	msg.Effort[5] = torque[5]
-	msg.Effort[6] = torque[6]
 
 
