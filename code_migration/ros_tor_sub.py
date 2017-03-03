7382178e5ee69d81d9b3dc9eaf20b9590bc973f4
nsujumnong <nsujumnong@wpi.edu>
2017-03-03 15:21:13 -0500

    add more python-ros code

--- code_migration/ros_tor_sub.py
+++ code_migration/ros_tor_sub.py
@@ -6,7 +6,19 @@ from sensor_msgs.msg import JointState
 from cisst_msgs.msg import vctDoubleVec
 
 def callback(data):
-	
+	tup = data.data
+    rospy.loginfo(rospy.get_caller_id())
+    print (tup)
+    str = type(tup)
+    print(str)
+#    list(tup)
+#    str = type(tup)
+#    print(str)
+    msg.layout.dim = []
+    msg.layout.data_offset = 0
+    msg.data = tup
+    print(msg.data)
+    pub.publish(msg)
 
 
 
@@ -14,7 +26,7 @@ def tor_listener():
 
 	rospy.init_node('tor_listener', anonymous=True)
 
-	rospy.Subscriber('/dvrk/MTMR/got_current_joint_effort',vctDoubleVec, callback)
+	rospy.Subscriber('/dvrk/MTMR/get_current_joint_effort',vctDoubleVec, callback)
 
 	rospy.spin()
 
