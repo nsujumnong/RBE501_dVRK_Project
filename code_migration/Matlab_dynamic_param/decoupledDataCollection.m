e0f411b7ad3f958da816de51657c7d93f6ff241b
nsujumnong <nsujumnong@wpi.edu>
2017-05-06 18:55:35 -0400

    edited some more data collection code

--- code_migration/Matlab_dynamic_param/decoupledDataCollection.m
+++ code_migration/Matlab_dynamic_param/decoupledDataCollection.m
@@ -1,4 +1,6 @@
 function torque_data = decoupledDataCollection()
+    % This function is for data collection for each joint torque of the MTM
+    % (the joints are decoupled)
     sub_pos = subscriber('/dvrk/MTMR/state_joint_current');
     sub_tor = subscirber('/dvrk/MTMR/state_joint_current');
     pub_pos = publisher('/dvrk/MTMR/set_position_joint');
@@ -6,13 +8,17 @@ function torque_data = decoupledDataCollection()
     % reset the pose to home configuration
     q = [0,0,0,0,0,0,0];
     Set_Position(pub_pos,q)
-    
+    % set and record the torque data at different configurations
+    % Each time the data collection of one joint is done, the robot is 
+    % reset to the home pose and then move on to the next joint
     for i = 1:7
-        msg = rosmessage(sub_pos);
-        for j = 1:100
-            
-        end
-        
+        for j = 1:10
+            q(i) = q(i)+0.005;
+            Set_Postion(pub_pos,q)
+            for k = 1:50
+                msg = receive(sub_tor);
+                torque_data(j,k,i) = msg.Effort(i);
+            end            
+        end       
     end
-
 end
